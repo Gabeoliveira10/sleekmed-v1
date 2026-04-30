bbf3db06ba67584671096560c46a438779caeccd
Gabeoliveira10 <122951087+Gabeoliveira10@users.noreply.github.com>
2026-04-30 11:16:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Live'));
+app.listen(PORT, () => console.log('Server is live'));
